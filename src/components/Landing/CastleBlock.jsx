a6b10658dec6519fe2c0cde067c8c7cc341ba328
FrowningCat <vsavinkov60@gmail.com>
2024-04-02 10:52:20 +0300

    add img in landing

--- src/components/Landing/CastleBlock.jsx
+++ src/components/Landing/CastleBlock.jsx
@@ -1,10 +1,36 @@
 import './landing.css';
 import line from '../../assets/line.png';
+import angell from '../../assets/angel.png';
+import catll from '../../assets/catl.png';
 import Rectangle1 from '../../assets/Rectangle 1.png';
+import { useState, useEffect } from 'react';
 
 export default function CastleBlock() {
+    const [angel, setAngel] = useState('angel');
+    const [catl, setCatl] = useState('catl');
+
+    function scroll() {
+        if (window.scrollY < 250) {
+            setAngel('angelHidden');
+        } else if (window.scrollY > 250) {
+            setAngel('angel');
+        }
+
+        if (window.scrollY < 350) {
+            setCatl('catlHidden');
+        } else if (window.scrollY > 350) {
+            setCatl('catl');
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', scroll);
+    });
+
     return (
         <div className="landing__castleBlock">
+            <img src={angell} alt="angel" className={angel} />
+            <img src={catll} alt="catl" className={catl} />
             <div className="landing__castleBlock__blokContext">
                 <p className="landing__castleBlock__blokContext__title">
                     ЗАМКИ

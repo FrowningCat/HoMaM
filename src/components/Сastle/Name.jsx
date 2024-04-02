41d6d7eca282428dafff4e810a15017c49b38fe6
FrowningCat <vsavinkov60@gmail.com>
2024-04-02 16:02:25 +0300

    small fix

--- src/components/Сastle/Name.jsx
+++ src/components/Сastle/Name.jsx
@@ -2,6 +2,7 @@ import './castle.css';
 import castl from '../../assets/1.jpg';
 import Group146 from '../../assets/Group 146.png';
 import Group147 from '../../assets/Group 147.png';
+import Group145 from '../../assets/Group 145.png';
 
 export default function Name() {
     return (
@@ -16,6 +17,7 @@ export default function Name() {
                 />
             </div>
             <img src={Group147} alt="Group147" className="imgRight" />
+            <img src={Group145} alt="Group145" className="imgBottom" />
         </>
     );
 }
